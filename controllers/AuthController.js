e0f0e812ab3716689dcfbf5e7e25df4274a3866a
Ali19Shamlooh <3looy19shamlooh@gmail.com>
2025-12-23 19:23:34 +0300

    finished auth

--- controllers/AuthController.js
+++ controllers/AuthController.js
@@ -1,53 +1,57 @@
-const { User } = require('../models')
-const middleware = require('../middleware')
-
-const { User } = require('../models')
-const { use } = require('react')
-
+const middleware = require("../middleware")
+const User = require("../models/user")
 const Register = async (req, res) => {
   try {
-    const {name, password} = req.body
+    const { name, password } = req.body
     let passwordDigest = await middleware.hashPassword(password)
-
-    let existingUser = await User.exists({ name })
+    let existingUser = await User.findOne({ name })
     if (existingUser) {
-        return res.status(400).send("This user is existed")
+      return res
+        .status(400)
+        .send({ status: "Error", msg: "This user already exists" })
     }
-    else {
-        const user = await User.create({ name, passwordDigest })
-        res.send(user)
-    }
+    const user = await User.create({ name, passwordDigest })
+    res.send(user)
   } catch (error) {
-    throw error
+    console.log(error)
+    res.status(500).send({ status: "Error", msg: "Registration failed" })
   }
 }
-
 const Login = async (req, res) => {
   try {
     const { name, password } = req.body
     const user = await User.findOne({ name })
-
+    if (!user) {
+      return res.status(401).send({ status: "Error", msg: "User not found" })
+    }
     let matched = await middleware.comparePassword(
-        password,
-        user.passwordDigest
+      password,
+      user.passwordDigest
     )
     if (matched) {
-        let payload = {
-            id: user._id,
-            name: user.name
-        }
-        let token = middleware.createToken(payload)
-        return res.send({user: payload, token})
+      let payload = {
+        id: user._id,
+        name: user.name,
+      }
+      let token = middleware.createToken(payload)
+      return res.send({ user: payload, token })
     }
-    res.status(401).send({status: 'Error', msg: 'unauthorized'})
+    res.status(401).send({ status: "Error", msg: "Invalid password" })
   } catch (error) {
     console.log(error)
-    res.status(401).send({status: 'error', msg: 'error'})
-    
+    res.status(401).send({ status: "error", msg: "Login error" })
   }
 }
-
+const CheckSession = async (req, res) => {
+  const { payload } = res.locals
+  res.send(payload)
+}
 module.exports = {
   Register,
+<<<<<<< HEAD
   Login
+=======
+  Login,
+  CheckSession,
+>>>>>>> f4be744 (finished auth)
 }
