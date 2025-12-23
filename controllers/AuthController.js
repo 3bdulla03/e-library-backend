8f298ccb46e87356c2b831165092f7dab9c18397
3bdulla03 <abdulla7545@gmail.com>
2025-12-23 15:32:58 +0300

    user register backend

--- controllers/AuthController.js
+++ controllers/AuthController.js
@@ -1,53 +1,35 @@
-const middleware = require("../middleware")
-const User = require("../models/user")
+const { User } = require('../models')
+const middleware = require('../middleware')
+
+const { User } = require('../models')
+
 const Register = async (req, res) => {
   try {
-    const { name, password } = req.body
+    const {name, password} = req.body
     let passwordDigest = await middleware.hashPassword(password)
-    let existingUser = await User.findOne({ name })
+
+    let existingUser = await User.exists({ name })
     if (existingUser) {
-      return res
-        .status(400)
-        .send({ status: "Error", msg: "This user already exists" })
+        return res.status(400).send("This user is existed")
     }
-    const user = await User.create({ name, passwordDigest })
-    res.send(user)
+    else {
+        const user = await User.create({ name, passwordDigest })
+        res.send(user)
+    }
   } catch (error) {
-    console.log(error)
-    res.status(500).send({ status: "Error", msg: "Registration failed" })
+    throw error
   }
 }
+
 const Login = async (req, res) => {
   try {
-    const { name, password } = req.body
-    const user = await User.findOne({ name })
-    if (!user) {
-      return res.status(401).send({ status: "Error", msg: "User not found" })
-    }
-    let matched = await middleware.comparePassword(
-      password,
-      user.passwordDigest
-    )
-    if (matched) {
-      let payload = {
-        id: user._id,
-        name: user.name,
-      }
-      let token = middleware.createToken(payload)
-      return res.send({ user: payload, token })
-    }
-    res.status(401).send({ status: "Error", msg: "Invalid password" })
+
   } catch (error) {
-    console.log(error)
-    res.status(401).send({ status: "error", msg: "Login error" })
+    throw error
   }
 }
-const CheckSession = async (req, res) => {
-  const { payload } = res.locals
-  res.send(payload)
-}
+
 module.exports = {
   Register,
-  Login,
-  CheckSession,
+  Login
 }
